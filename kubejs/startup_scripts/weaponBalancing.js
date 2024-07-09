757a958e36426f4aeb20d3a1d20f3530c338ecc4
qxZap <mileamihai@outlook.it>
2024-07-09 19:54:54 +0300

    balanded blue skies sets and modified kubejs

--- kubejs/startup_scripts/weaponBalancing.js
+++ kubejs/startup_scripts/weaponBalancing.js
@@ -3,55 +3,6 @@
 // MC: 1.20.1
 
 ItemEvents.modification( (event) => {
-    // Blue Skies
-    event.modify("blue_skies:bluebright_sword", item => {
-        item.attackDamage = 5;
-    });
-    event.modify("blue_skies:starlit_sword", item => {
-        item.attackDamage = 5;
-    });
-    event.modify("blue_skies:frostbright_sword", item => {
-        item.attackDamage = 5;
-    });
-    event.modify("blue_skies:lunar_sword", item => {
-        item.attackDamage = 5;
-    });
-    event.modify("blue_skies:dusk_sword", item => {
-        item.attackDamage = 5;
-    });
-    event.modify("blue_skies:maple_sword", item => {
-        item.attackDamage = 5;
-    });
-    event.modify("blue_skies:comet_sword", item => {
-        item.attackDamage = 6;
-    });
-    event.modify("blue_skies:turquoise_stone_sword", item => {
-        item.attackDamage = 6.5;
-    });
-    event.modify("blue_skies:lunar_stone_sword", item => {
-        item.attackDamage = 6.5;
-    });
-    event.modify("blue_skies:pyrope_sword", item => {
-        item.attackDamage = 8;
-    });
-    event.modify("blue_skies:aquite_sword", item => {
-        item.attackDamage = 100;
-        item.attackSpeed = 2;
-        item.rarity = "RARE"
-    });
-    event.modify("blue_skies:diopside_sword", item => {
-        item.attackDamage = 12.5;
-    });
-    event.modify("blue_skies:chaoite_sword", item => {
-        item.attackDamage = 11;
-    });
-    event.modify("blue_skies:horizonite_sword", item => {
-        item.attackDamage = 10;
-    });
-    event.modify("blue_skies:different_sword", item => {
-        item.attackDamage = 15;
-    });
-
     // Aether
     event.modify("aether:skyroot_sword", item => {
         item.attackDamage = 8;
