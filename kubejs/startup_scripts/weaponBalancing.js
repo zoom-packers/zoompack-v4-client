15f5d686ed4fd802e1b274b2aa83effadaa3bfc5
qxZap <mileamihai@outlook.it>
2024-07-09 23:40:59 +0300

    aether balance + progression

--- kubejs/startup_scripts/weaponBalancing.js
+++ kubejs/startup_scripts/weaponBalancing.js
@@ -4,30 +4,7 @@
 
 ItemEvents.modification( (event) => {
     // Aether
-    event.modify("aether:skyroot_sword", item => {
-        item.attackDamage = 8;
-    });
-    event.modify("aether:holystone_sword", item => {
-        item.attackDamage = 9;
-    });
-    event.modify("aether:zanite_sword", item => {
-        item.attackDamage = 15;
-    });
-    event.modify("aether:gravitite_sword", item => {
-        item.attackDamage = 20;
-    });
-    event.modify("aether:candy_cane_sword", item => {
-        item.attackDamage = 8;
-    });
-    event.modify("aether:holy_sword", item => {
-        item.attackDamage = 20;
-    });
-    event.modify("aether:lightning_sword", item => {
-        item.attackDamage = 20;
-    });
-    event.modify("aether:flaming_sword", item => {
-        item.attackDamage = 20;
-    });
+    
 
     // Nether
     event.modify("minecraft:netherite_sword", item => {
