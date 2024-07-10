fe96441c30d3916574c01032899701b86e887b85
qxZap <mileamihai@outlook.it>
2024-07-10 18:13:53 +0300

    nevermind

--- kubejs/server_scripts/obsidian_armor_recipes.js
+++ kubejs/server_scripts/obsidian_armor_recipes.js
@@ -1,26 +1,26 @@
 ServerEvents.recipes(event => {
-    event.smithing(
-        'aether:obsidian_helmet',
-        'compressium:obsidian_1',
-        'aether:phoenix_helmet',
-        'compressium:obsidian_2'
-    );
-    event.smithing(
-        'aether:obsidian_chestplate',
-        'compressium:obsidian_1',
-        'aether:phoenix_chestplate',
-        'compressium:obsidian_2'
-    );
-    event.smithing(
-        'aether:obsidian_leggings',
-        'compressium:obsidian_1',
-        'aether:phoenix_leggings',
-        'compressium:obsidian_2'
-    );
-    event.smithing(
-        'aether:obsidian_boots',
-        'compressium:obsidian_1',
-        'aether:phoenix_boots',
-        'compressium:obsidian_2'
-    );
+    // event.smithing(
+    //     'aether:obsidian_helmet',
+    //     'compressium:obsidian_1',
+    //     'aether:phoenix_helmet',
+    //     'compressium:obsidian_2'
+    // );
+    // event.smithing(
+    //     'aether:obsidian_chestplate',
+    //     'compressium:obsidian_1',
+    //     'aether:phoenix_chestplate',
+    //     'compressium:obsidian_2'
+    // );
+    // event.smithing(
+    //     'aether:obsidian_leggings',
+    //     'compressium:obsidian_1',
+    //     'aether:phoenix_leggings',
+    //     'compressium:obsidian_2'
+    // );
+    // event.smithing(
+    //     'aether:obsidian_boots',
+    //     'compressium:obsidian_1',
+    //     'aether:phoenix_boots',
+    //     'compressium:obsidian_2'
+    // );
 });
