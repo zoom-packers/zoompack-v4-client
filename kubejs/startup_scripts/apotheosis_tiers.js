678ba43831de96af4b486eca2eb445917ef87f15
panda <pandaismyname1@gmail.com>
2024-07-25 00:40:55 +0300

    More balancing after playthrough. Removed WorldEdit, Pehkui

--- kubejs/startup_scripts/apotheosis_tiers.js
+++ kubejs/startup_scripts/apotheosis_tiers.js
@@ -9,5 +9,5 @@ StartupEvents.registry("item", e => {
     createRarityMaterial(e, "apoth_extra_tier_0", "Milea's Sweat", "kubejs:item/apoth_extra_tier_0")
     createRarityMaterial(e, "apoth_extra_tier_1", "Calin's Torment", "kubejs:item/apoth_extra_tier_1")
     createRarityMaterial(e, "apoth_extra_tier_2", "Marius's Tears", "kubejs:item/apoth_extra_tier_2")
-    createRarityMaterial(e, "apoth_extra_tier_3", "Emi's Cum", "kubejs:item/apoth_extra_tier_3")
+    createRarityMaterial(e, "apoth_extra_tier_3", "Emi's Blood", "kubejs:item/apoth_extra_tier_3")
 })
