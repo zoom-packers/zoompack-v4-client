aa4d2dd8a289c7c57e845709dbd158ccc9ec3a38
qxZap <mileamihai@outlook.it>
2024-08-23 00:04:16 +0300

    removed old imp for ban abyss book | mixin tati required

--- kubejs/server_scripts/advjs_server.js
+++ kubejs/server_scripts/advjs_server.js
@@ -1,32 +1,3 @@
 AdvJSEvents.advancement(event => {
     event.remove("irons_spellbooks:grant_patchouli");
 });
-
-let PATCHULI_BOOK_ITEM_ID = 'patchouli:guide_book';
-let PERFORMED_FIRST_JOIN_TAG = 'PERFORMED_FIRST_JOIN';
-
-function playerHasTag(tags, searched_tag){
-    let found = false;
-    tags.forEach(tag => {
-        if(tag.toString() === searched_tag){
-            found = true;
-        }
-    });
-    
-    return found;
-}
-
-PlayerEvents.inventoryChanged(event => {
-    let item_id = event.item.getItem().getId();
-    let player = event.player;
-    if(player!=null){
-        if(item_id === PATCHULI_BOOK_ITEM_ID){
-            let tags = event.player.tags;
-            if(!playerHasTag(tags, PERFORMED_FIRST_JOIN_TAG)){
-                let player_name = player.name.string;
-                event.server.runCommandSilent(`/clear ${player_name} ${PATCHULI_BOOK_ITEM_ID}`);
-                event.server.runCommandSilent(`/tag ${player_name} add ${PERFORMED_FIRST_JOIN_TAG}`);
-            }
-        }
-    }
-});
