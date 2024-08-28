8bfb25a384022c9d1eaa0ae5db84cbad24fff332
qxZap <mileamihai@outlook.it>
2024-08-29 00:16:39 +0300

    more tips and tricks

--- kubejs/server_scripts/helper_commands/help.js
+++ kubejs/server_scripts/helper_commands/help.js
@@ -1,22 +1,34 @@
-const COMMANDS_FTBQ_BOOKS_MAP = {
-    'help' : '39A979E3A6CE5B99',
-    'gems' : '37AB552495798316',
-    'mounts' : '1F7911B301774426',
-    'enchanting' : '0FDCC38C52EC2B0A',
-    'levels' : '301C03DAB5AE0E67',
-    'leveling' : '301C03DAB5AE0E67',
-    'events': '2D72E4DD3F561D0B'
+function ftb_book_command(book_id){
+    return `ftbquests open_book ${book_id}`;
+}
+
+let COMMANDS_CUSTOM_MAP = {
+    'DailyRewards' : 'DailyRewards claim',
+    'dailyrewards' : 'DailyRewards claim',
+    'daily_rewards' : 'DailyRewards claim',
+    'daily' : 'DailyRewards claim',
+    'help' : ftb_book_command('39A979E3A6CE5B99'),
+    'gems' : ftb_book_command('37AB552495798316'),
+    'mounts' : ftb_book_command('1F7911B301774426'),
+    'enchanting' : ftb_book_command('0FDCC38C52EC2B0A'),
+    'levels' : ftb_book_command('301C03DAB5AE0E67'),
+    'leveling' : ftb_book_command('301C03DAB5AE0E67'),
+    'events': ftb_book_command('2D72E4DD3F561D0B'),
+    'economy': ftb_book_command('547C6F5B2EC94450'),
+    'trims': ftb_book_command('089EF30AFF6A1D46'),
+    'powerups': ftb_book_command('089EF30AFF6A1D46'),
+    'talents': ftb_book_command('70CF500D7065E04A'),
+    'dimensions': ftb_book_command('5C849DD6772F7444')
+
 }
 
 ServerEvents.commandRegistry(event => {
     const {commands: Commands, arguments: Arguments} = event;
-    Object.entries(COMMANDS_FTBQ_BOOKS_MAP).forEach(([commandName, bookId]) => {
+    Object.entries(COMMANDS_CUSTOM_MAP).forEach(([commandName, command]) => {
         event.register(
             Commands.literal(commandName)
                 .executes(ctx => {
-                    const playerName = ctx.source.player.name.string;
-                    const command = `ftbquests open_book ${bookId}`;
-                    ctx.source.server.runCommandSilent(`execute as ${playerName} run ${command}`);
+                    ctx.source.server.runCommandSilent(`execute as ${ctx.source.player.name.string} run ${command}`);
                     return 1;
                 })
         );
