a0ffcbf9f7901e79764b68b923d4f9bfe6d3f074
panda <pandaismyname1@gmail.com>
2024-08-30 22:31:34 +0300

    Zoompack introduction

---
+++ kubejs/server_scripts/join_server/introductionToModpack.js
@@ -0,0 +1,3 @@
+PlayerEvents.loggedIn((event) => {
+    event.player.tell('§eWelcome to §5§lZoompack 4§r§r! §eThis modpack is heavily changing vanilla mechanics & adds a lot of new ones, so we suggest you type §r§o§b/help§r§r§e to get started. Have fun!§r');
+});
