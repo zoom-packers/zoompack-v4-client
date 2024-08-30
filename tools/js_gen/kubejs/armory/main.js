c74c35353a7bd23d853c24c2fb6cbff6491de23a
qxZap <mileamihai@outlook.it>
2024-08-31 01:02:27 +0300

    recent changes and rebalance on existing armory

--- tools/js_gen/kubejs/armory/main.js
+++ tools/js_gen/kubejs/armory/main.js
@@ -43,45 +43,57 @@ const materials = [
     {item: item_minecraft.i_iron_ingot, type: "crafting", "name": "iron", base_damage: 1, durability: 250, armor: 1, pmmoLevel: 9, materialColor: "#C0C0C0"},
     {item: item_minecraft.i_gold_ingot, type: "crafting", name: "gold", base_damage: 1.5, durability: 50, armor: 1.5, pmmoLevel: 12, materialColor: "#FFD700"},
     {item: item_minecraft.i_diamond, type: "crafting", name: "diamond", base_damage: 2, durability: 500, armor: 2, pmmoLevel: 18, materialColor: "#00FFFF"},
-    {item: item_minecraft.i_netherite_ingot, type: "smithing", name: "netherite", smithing: item_minecraft.i_netherite_upgrade_smithing_template, base: "diamond", base_damage: 25, durability: 1650, armor: 15, pmmoLevel: 58, materialColor: "#282224"},
-    {item: item_blue_skies.i_pyrope_gem, type: "crafting", name: "pyrope", base_damage: 2, durability: 800, armor: 2.5, pmmoLevel: 21, materialColor: "#e61542"},
-    {item: item_blue_skies.i_aquite, type: "crafting", name: "aquite", base_damage: 2.5, durability: 850, armor: 2.75, pmmoLevel: 23, materialColor: "#5aeaea"},
-    {item: item_blue_skies.i_diopside_gem, type: "crafting", name: "diopside", base_damage: 2.7, durability: 900, armor: 3, pmmoLevel: 25, materialColor: "#35bf35"},
-    {item: item_blue_skies.i_charoite, type: "crafting", name: "charoite", base_damage: 3, durability: 950, armor: 3.5, pmmoLevel: 29, materialColor: "#bf35af"},
-    {item: item_blue_skies.i_horizonite_ingot, type: "crafting", name: "horizonite", base_damage: 4.5, durability: 1000, armor: 4.5, pmmoLevel: 32, materialColor: "#FFA500"},
-    {item: item_aether.i_zanite_gemstone, type: "crafting", name: "zanite", base_damage: 5, durability: 1100, armor: 5, pmmoLevel: 35, materialColor: "#75108a"},
-    {item: item_aether.i_enchanted_gravitite, type: "crafting", name: "enchanted_gravitite", base_damage: 6, durability: 1200, armor: 6, pmmoLevel: 39, materialColor: "#8a1053"},
-    {item: item_betternether.i_cincinnasite_ingot, type: "crafting", name: "cincinnasite", base_damage: 11, durability: 1300, armor: 11, pmmoLevel: 52, materialColor: "#c28721"},
-    {item: item_minecraft.i_diamond, type: "smithing", name: "cincinnasite_diamond", smithing: item_betternether.i_cincinnasite_diamond_upgrade_smithing_template, "base": "cincinnasite", base_damage: 12, durability: 1400, armor: 12, pmmoLevel: 54, materialColor: "#764e1e"},
-    {item: item_betternether.i_nether_ruby, type: "crafting", name: "nether_ruby", base_damage: 15, durability: 1500, armor: 15, pmmoLevel: 56, materialColor: "#FF0000"},
-    {item: item_minecraft.i_sculk_catalyst, type: "smithing", name: "flaming_ruby", smithing: item_betternether.i_flaming_ruby_upgrade_smithing_template, "base": "nether_ruby", base_damage: 19, durability: 1600, armor: 19, pmmoLevel: 57, materialColor: "#e50e6a"},
-    {item: item_undergarden.i_cloggrum_ingot, type: "crafting", name: "cloggrum", base_damage: 55, durability: 1700, armor: 55, pmmoLevel: 61, materialColor: "#b1a17b"},
-    {item: item_undergarden.i_froststeel_ingot, type: "crafting", name: "froststeel", base_damage: 65, durability: 1800, armor: 65, pmmoLevel: 63, materialColor: "#3786aa"},
-    {item: item_undergarden.i_utherium_crystal, type: "crafting", name: "utherium", base_damage: 75, durability: 1900, armor: 75, pmmoLevel: 65, materialColor: "#831414"},
-    {item: item_undergarden.i_forgotten_ingot, type: "smithing", name: "forgotten", smithing: item_undergarden.i_forgotten_upgrade_smithing_template, "base": "utherium", base_damage: 85, durability: 2000, armor: 85, pmmoLevel: 67, materialColor: "#29ae63"},
-    {item: item_call_of_yucutan.i_jade, type: "crafting", name: "jade", base_damage: 80, durability: 2000, armor: 80, pmmoLevel: 69, materialColor: "#6bfbbe"},
-    // darker green pls
+    // {item: item_minecraft.i_diamond, type: "crafting", name: "neptunium", base_damage: 2, durability: 1000, armor: 2, pmmoLevel: 19, materialColor: "#00FFFF"},
 
+    // Add neptune
 
-    // purple tint slightly
-    {item: item_phantasm.i_crystal_block, type: "crafting", name: "crystalline", base_damage: 100, durability: 2200, armor: 100, pmmoLevel: 73, materialColor: "#f3e0ef"},
-    // negro
-    {item: item_ender_dragon_loot_.i_dragon_scale, type: "smithing", name: "dragon", base_damage: 127, durability: 2400, armor: 127, pmmoLevel: 75, materialColor: "#ca01f6"},
-    {item: item_enlightened_end.i_adamantite_ingot, type: "smithing", name: "adamantite", base_damage: 157, durability: 2600, armor: 157, pmmoLevel: 77, materialColor: "#4a7dfd"},
-    {item: item_enderitemod.i_enderite_ingot, type: "smithing", name: "enderite", base_damage: 186, durability: 2800, armor: 186, pmmoLevel: 79, materialColor: "#1b564d"},
-    // disable the ones in the mod
-
-    {item: item_callfromthedepth_.i_immemorialingot, type: "crafting", name: "immemorial", base_damage: 200, durability: 2800, armor: 200, pmmoLevel: 84, materialColor: "#629592"},
-    {item: item_theabyss.i_fixed_bone, type: "crafting", name: "fixed_bone", base_damage: 239, durability: 3200, armor: 239, pmmoLevel: 90, materialColor: "#c6c6c6"},
-    {item: item_theabyss.i_fusion_ingot, type: "crafting", name: "fusion", base_damage: 223, durability: 3400, armor: 223, pmmoLevel: 91, materialColor: "#39ebc7"},
-    {item: item_theabyss.i_aberythe_gem, type: "crafting", name: "aberythe", base_damage: 232, durability: 3600, armor: 232, pmmoLevel: 92, materialColor: "#451e63"},
-    {item: item_theabyss.i_ignisithe_gem, type: "crafting", name: "ignisithe", base_damage: 262, durability: 3800, armor: 262, pmmoLevel: 93, materialColor: "#33c0a4"},
-    {item: item_theabyss.i_glacerythe_gem, type: "crafting", name: "glacerythe", base_damage: 285, durability: 4000, armor: 285, pmmoLevel: 94, materialColor: "#2b9d86"},
-    {item: item_theabyss.i_garnite_ingot, type: "crafting", name: "garnite", base_damage: 302, durability: 4200, armor: 302, pmmoLevel: 95, materialColor: "#268251"},
-    {item: item_theabyss.i_phantom_ingot, type: "crafting", name: "phantom", base_damage: 324, durability: 4400, armor: 324, pmmoLevel: 96, materialColor: "#0f5226"},
-    {item: item_theabyss.i_unorithe_ingot, type: "crafting", name: "unorithe", base_damage: 355, durability: 4600, armor: 355, pmmoLevel: 97, materialColor: "#092a23"},
+    // Blue skies
+    {item: item_blue_skies.i_pyrope_gem, type: "crafting", name: "pyrope", base_damage: 2, durability: 1600, armor: 2.5, pmmoLevel: 21, materialColor: "#e61542"},
+    {item: item_blue_skies.i_aquite, type: "crafting", name: "aquite", base_damage: 2.5, durability: 1700, armor: 2.75, pmmoLevel: 23, materialColor: "#5aeaea"},
+    {item: item_blue_skies.i_diopside_gem, type: "crafting", name: "diopside", base_damage: 2.7, durability: 1800, armor: 3, pmmoLevel: 25, materialColor: "#35bf35"},
+    {item: item_blue_skies.i_charoite, type: "crafting", name: "charoite", base_damage: 3, durability: 1900, armor: 3.5, pmmoLevel: 29, materialColor: "#bf35af"},
+    {item: item_blue_skies.i_horizonite_ingot, type: "crafting", name: "horizonite", base_damage: 4.5, durability: 2000, armor: 4.5, pmmoLevel: 32, materialColor: "#FFA500"},
+    
+    // Aether
+    {item: item_aether.i_zanite_gemstone, type: "crafting", name: "zanite", base_damage: 5, durability: 2211, armor: 5, pmmoLevel: 35, materialColor: "#75108a"},
+    {item: item_aether.i_enchanted_gravitite, type: "crafting", name: "enchanted_gravitite", base_damage: 6, durability: 2431, armor: 6, pmmoLevel: 39, materialColor: "#8a1053"},
+    
+    // Nether
+    {item: item_betternether.i_cincinnasite_ingot, type: "crafting", name: "cincinnasite", base_damage: 11, durability: 2700, armor: 11, pmmoLevel: 52, materialColor: "#c28721"},
+    {item: item_minecraft.i_diamond, type: "smithing", name: "cincinnasite_diamond", smithing: item_betternether.i_cincinnasite_diamond_upgrade_smithing_template, "base": "cincinnasite", base_damage: 12, durability: 2800, armor: 12, pmmoLevel: 54, materialColor: "#764e1e"},
+    {item: item_betternether.i_nether_ruby, type: "crafting", name: "nether_ruby", base_damage: 15, durability: 2950, armor: 15, pmmoLevel: 56, materialColor: "#FF0000"},
+    {item: item_minecraft.i_sculk_catalyst, type: "smithing", name: "flaming_ruby", smithing: item_betternether.i_flaming_ruby_upgrade_smithing_template, "base": "nether_ruby", base_damage: 19, durability: 3025, armor: 19, pmmoLevel: 57, materialColor: "#e50e6a"},
+    {item: item_minecraft.i_netherite_ingot, type: "smithing", name: "netherite", smithing: item_minecraft.i_netherite_upgrade_smithing_template, base: "diamond", base_damage: 25, durability: 3100, armor: 15, pmmoLevel: 58, materialColor: "#282224"},
+    
+    // Undergarden
+    {item: item_undergarden.i_cloggrum_ingot, type: "crafting", name: "cloggrum", base_damage: 55, durability: 3300, armor: 55, pmmoLevel: 61, materialColor: "#b1a17b"},
+    {item: item_undergarden.i_froststeel_ingot, type: "crafting", name: "froststeel", base_damage: 65, durability: 3500, armor: 65, pmmoLevel: 63, materialColor: "#3786aa"},
+    {item: item_undergarden.i_utherium_crystal, type: "crafting", name: "utherium", base_damage: 75, durability: 3700, armor: 75, pmmoLevel: 65, materialColor: "#831414"},
+    {item: item_undergarden.i_forgotten_ingot, type: "smithing", name: "forgotten", smithing: item_undergarden.i_forgotten_upgrade_smithing_template, "base": "utherium", base_damage: 85, durability: 4100, armor: 85, pmmoLevel: 67, materialColor: "#29ae63"},
+    {item: item_call_of_yucutan.i_jade, type: "crafting", name: "jade", base_damage: 80, durability: 4000, armor: 80, pmmoLevel: 69, materialColor: "#2f7d5c"},
+    
+    // End
+    {item: item_phantasm.i_crystal_block, type: "crafting", name: "crystalline", base_damage: 100, durability: 4600, armor: 100, pmmoLevel: 73, materialColor: "#50437a"},
+    {item: item_ender_dragon_loot_.i_dragon_scale, type: "smithing", name: "dragon", smithing: item_ender_dragon_loot_.i_dragon_upgrade_template, "base": "netherite", base_damage: 127, durability: 4800, armor: 127, pmmoLevel: 75, materialColor: "#343434"},
+    {item: item_enlightened_end.i_adamantite_ingot, type: "smithing", name: "adamantite", smithing: item_enlightened_end.i_adamantite_smithing_template, "base": "diamond", base_damage: 157, durability: 5200, armor: 157, pmmoLevel: 77, materialColor: "#4a7dfd"},
+    {item: item_enderitemod.i_enderite_ingot, type: "smithing", name: "enderite", smithing: item_enderitemod.i_enderite_upgrade_smithing_template, "base": "netherite", base_damage: 186, durability: 5500, armor: 186, pmmoLevel: 79, materialColor: "#1b564d"},
+    
+    // Call from the depth
+    {item: item_callfromthedepth_.i_immemorialingot, type: "crafting", name: "immemorial", base_damage: 200, durability: 5700, armor: 200, pmmoLevel: 84, materialColor: "#629592"},
+    {item: item_callfromthedepth_.i_soulingot, type: "smithing", name: "soul", smithing: item_callfromthedepth_.i_immemorialupgradesmithingtemplate, "base": "depth", base_damage: 220, durability: 6000, armor: 220, pmmoLevel: 88, materialColor: "#5474b4", skip:["bow"]},
+    
+    // Abyss
+    {item: item_theabyss.i_fixed_bone, type: "crafting", name: "fixed_bone", base_damage: 239, durability: 6100, armor: 239, pmmoLevel: 90, materialColor: "#c6c6c6"},
+    {item: item_theabyss.i_fusion_ingot, type: "crafting", name: "fusion", base_damage: 223, durability: 6250, armor: 223, pmmoLevel: 91, materialColor: "#39ebc7"},
+    {item: item_theabyss.i_aberythe_gem, type: "crafting", name: "aberythe", base_damage: 232, durability: 6500, armor: 232, pmmoLevel: 92, materialColor: "#451e63"},
+    {item: item_theabyss.i_ignisithe_gem, type: "crafting", name: "ignisithe", base_damage: 262, durability: 6750, armor: 262, pmmoLevel: 93, materialColor: "#33c0a4"},
+    {item: item_theabyss.i_fixed_bone, type: "crafting", name: "knight", base_damage: 272, durability: 6875, armor: 272, pmmoLevel: 94, materialColor: "#969190"},
+    {item: item_theabyss.i_glacerythe_gem, type: "crafting", name: "glacerythe", base_damage: 285, durability: 7000, armor: 285, pmmoLevel: 94, materialColor: "#2b9d86"},
+    {item: item_theabyss.i_garnite_ingot, type: "crafting", name: "garnite", base_damage: 302, durability: 7250, armor: 302, pmmoLevel: 95, materialColor: "#268251"},
+    {item: item_theabyss.i_phantom_ingot, type: "crafting", name: "phantom", base_damage: 324, durability: 7500, armor: 324, pmmoLevel: 96, materialColor: "#0f5226"},
+    {item: item_theabyss.i_unorithe_ingot, type: "crafting", name: "unorithe", base_damage: 355, durability: 7750, armor: 355, pmmoLevel: 97, materialColor: "#092a23"},
+    {item: item_theabyss.i_incorythe_gem, type: "crafting", name: "incorythe", base_damage: 400, durability: 8000, armor: 400, pmmoLevel: 98, materialColor: "#72d5ca"},
 ]
-
 
 const weaponTypes = [
     {name: "dagger", recipe: ["","","","","material","","#forge:rods/wooden","",""], durabilityMultiplier: 0.9, damageMultiplier: 0.5, speedMultiplier: 0.5, textureGenType: "normal"},
@@ -116,6 +128,17 @@ const crossbowTypes = [
 const mergedTypes = [...weaponTypes, ...shieldTypes, ...bowTypes, ...crossbowTypes];
 const mergedArcheryTypes = [...bowTypes, ...crossbowTypes];
 
+function materialWithTypeShouldBeSkiped(verified_material, item_to_be_created_type){
+    let to_skip = [];
+
+    if ('skip' in verified_material){
+        to_skip = verified_material.skip;
+        console.log(to_skip);
+    }
+
+    return to_skip.includes(item_to_be_created_type);
+}
+
 //=====================
 // REGISTRATION
 //=====================
@@ -123,6 +146,9 @@ let materialIndex = 0;
 const startupContent = [];
 for (const material of materials) {
     for (const weaponType of weaponTypes) {
+        if(materialWithTypeShouldBeSkiped(material, weaponType)){
+            continue;
+        }
         const materialIdPart = material.name;
         const id = `${modId}:${materialIdPart}_${weaponType.name}`;
         const displayName = `${materialIdPart.toUpperCase()[0] + materialIdPart.slice(1).replace('_', ' ')} ${weaponType.name.toUpperCase()[0] + weaponType.name.slice(1)}`;
@@ -131,6 +157,9 @@ for (const material of materials) {
         startupContent.push(weaponTemplate.replace("{id}", id).replace("{displayName}", displayName).replace("{texture}", texture).replace("{durability}", durability));
     }
     for (const shieldType of shieldTypes) {
+        if(materialWithTypeShouldBeSkiped(material, shieldType)){
+            continue;
+        }
         const materialIdPart = material.name;
         const id = `${modId}:${materialIdPart}_${shieldType.name}`;
         const displayName = `${materialIdPart.toUpperCase()[0] + materialIdPart.slice(1)} ${shieldType.name.toUpperCase()[0] + shieldType.name.slice(1)}`;
@@ -139,6 +168,10 @@ for (const material of materials) {
         startupContent.push(shieldTemplate.replace("{id}", id).replace("{displayName}", displayName).replace("{texture}", texture).replace("{durability}", durability));
     }
     for (const bowType of bowTypes) {
+        if(materialWithTypeShouldBeSkiped(material, bowType)){
+            console.log("SKIPPING")
+            continue;
+        }
         const materialIdPart = material.name;
         const id = `${modId}:${materialIdPart}_${bowType.name}`;
         const displayName = `${materialIdPart.toUpperCase()[0] + materialIdPart.slice(1)} ${bowType.name.toUpperCase()[0] + bowType.name.slice(1)}`;
@@ -147,6 +180,9 @@ for (const material of materials) {
         startupContent.push(bowTemplate.replace("{id}", id).replace("{displayName}", displayName).replace("{texture}", texture).replace("{durability}", durability));
     }
     for (const crossbowType of crossbowTypes) {
+        if(materialWithTypeShouldBeSkiped(material, crossbowType)){
+            continue;
+        }
         const materialIdPart = material.name;
         const id = `${modId}:${materialIdPart}_${crossbowType.name}`;
         const displayName = `${materialIdPart.toUpperCase()[0] + materialIdPart.slice(1)} ${crossbowType.name.toUpperCase()[0] + crossbowType.name.slice(1)}`;
@@ -168,18 +204,27 @@ const cia = JSON.parse(fs.readFileSync(ciaPath, "utf8"));
 
 for (const material of materials) {
     for (const weaponType of weaponTypes) {
+        if(materialWithTypeShouldBeSkiped(material, weaponType)){
+            continue;
+        }
         const materialIdPart = material.name;
         const id = `${modId}:${materialIdPart}_${weaponType.name}`;
         const entry = createCiaWeapon(id, material, weaponType);
         addItemToCia(cia, entry);
     }
     for (const shieldType of shieldTypes) {
+        if(materialWithTypeShouldBeSkiped(material, shieldType)){
+            continue;
+        }
         const materialIdPart = material.name;
         const id = `${modId}:${materialIdPart}_${shieldType.name}`;
         const entry = createCiaShield(id, material, shieldType);
         addItemToCia(cia, entry);
     }
     for (const bowType of mergedArcheryTypes) {
+        if(materialWithTypeShouldBeSkiped(material, bowType)){
+            continue;
+        }
         const materialIdPart = material.name;
         const id = `${modId}:${materialIdPart}_${bowType.name}`;
         const entry = createCiaProjectileWeapon(id, material, bowType);
@@ -240,6 +285,9 @@ function createCiaShield(itemId, material, shieldType) {
 const recipes = [];
 for (const mergedType of mergedTypes) {
     for (const material of materials) {
+        if(materialWithTypeShouldBeSkiped(material, mergedType)){
+            continue;
+        }
         const materialIdPart = material.name;
         const id = `${modId}:${materialIdPart}_${mergedType.name}`;
         if (material.type === "crafting") {
