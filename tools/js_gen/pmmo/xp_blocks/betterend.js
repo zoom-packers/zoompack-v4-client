59facb9c9d5beb50ef07f773179ffafe2b7a8113
qxZap <mileamihai@outlook.it>
2024-08-30 23:23:36 +0300

    changed pmmo datapack generator for end

--- tools/js_gen/pmmo/xp_blocks/betterend.js
+++ tools/js_gen/pmmo/xp_blocks/betterend.js
@@ -1,18 +1,18 @@
 const {Template} = require('./template.js');
-const {item_betterend} = require("../../typedefs/item_typedefs");
+const {item_enlightened_end} = require("../../typedefs/item_typedefs");
 
 const malachite = Template.build()
-    .withTagFor('enlightened_end:malachite_ore')
+    .withTagFor(item_enlightened_end.i_malachite_ore)
     .withBreakXpValue('mining', 750)
     .withPlaceXpValue('mining', 10);
 
 const irradium = Template.build()
-    .withTagFor('enlightened_end:irradium_ore')
+    .withTagFor(item_enlightened_end.i_irradium_ore)
     .withBreakXpValue('mining', 850)
     .withPlaceXpValue('mining', 10);
 
 const bismuth = Template.build()
-    .withTagFor('enlightened_end:bismuth_ore')
+    .withTagFor(item_enlightened_end.i_bismuth_ore)
     .withBreakXpValue('mining', 850)
     .withPlaceXpValue('mining', 10);
 
@@ -21,8 +21,6 @@ const ores = [
     irradium,
     bismuth
 ];
-
-
 
 module.exports = {
     ores
