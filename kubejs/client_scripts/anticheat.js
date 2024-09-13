e68d8965601485b709b2c695380210078b782792
panda <pandaismyname1@gmail.com>
2024-09-13 05:43:39 +0300

    Client Anticheat batching data

--- kubejs/client_scripts/anticheat.js
+++ kubejs/client_scripts/anticheat.js
@@ -23,16 +23,51 @@ function getResourcePackList() {
     });
 }
 
-function buildServerPackage() {
+function buildServerPackages() {
     const modList = getModList();
     const resourcePackList = getResourcePackList();
+    const batchCount = 100;
+    const modBatches = [];
+    const resourcePackBatches = [];
+    for (let i = 0; i < modList.length; i += batchCount) {
+        modBatches.push(modList.slice(i, i + batchCount));
+    }
+    for (let i = 0; i < resourcePackList.length; i += batchCount) {
+        resourcePackBatches.push(resourcePackList.slice(i, i + batchCount));
+    }
     return {
-        mods: modList,
-        resourcePacks: resourcePackList
+        modBatches: modBatches,
+        resourcePackBatches: resourcePackBatches
     };
 }
 
 NetworkEvents.dataReceived('anticheat:check', function (event) {
-    const serverPackage = buildServerPackage();
-    event.getEntity().sendData('anticheat:response', serverPackage);
+    const batches = buildServerPackages();
+    const minBatches = Math.min(batches.modBatches.length, batches.resourcePackBatches.length);
+    const maxBatches = Math.max(batches.modBatches.length, batches.resourcePackBatches.length);
+    const mostBatches = batches.modBatches.length > batches.resourcePackBatches.length ? 'mods' : 'resource packs';
+    for (let i = 0; i < minBatches; i++) {
+        const serverPackage = {
+            mods: batches.modBatches[i],
+            resourcePacks: batches.resourcePackBatches[i]
+        };
+        event.getEntity().sendData('anticheat:response', serverPackage);
+    }
+    if (mostBatches === 'mods') {
+        for (let i = minBatches; i < maxBatches; i++) {
+            const serverPackage = {
+                mods: batches.modBatches[i],
+                resourcePacks: []
+            };
+            event.getEntity().sendData('anticheat:response', serverPackage);
+        }
+    } else {
+        for (let i = minBatches; i < maxBatches; i++) {
+            const serverPackage = {
+                mods: [],
+                resourcePacks: batches.resourcePackBatches[i]
+            };
+            event.getEntity().sendData('anticheat:response', serverPackage);
+        }
+    }
 });
