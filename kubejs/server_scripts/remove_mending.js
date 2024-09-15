4fb22673f57e54b94be932a8b693abfe9c27dbbc
panda <pandaismyname1@gmail.com>
2024-09-15 07:20:33 +0300

    Remove mending script fix

--- kubejs/server_scripts/remove_mending.js
+++ kubejs/server_scripts/remove_mending.js
@@ -1,17 +1,22 @@
 PlayerEvents.inventoryChanged(e => {
     let itemStack = e.getItem();
     let enchantmentTags = itemStack.getEnchantmentTags();
+    let removedMending = false;
     for (let i = 0; i < enchantmentTags.size(); i++) {
         let enchantmentTag = enchantmentTags.get(i);
         let id = enchantmentTag.getString("id");
         if (id === "minecraft:mending") {
             enchantmentTags.remove(i);
+            removedMending = true;
             i--;
         }
         if (id === "apotheosis:life_mending") {
             enchantmentTags.remove(i);
+            removedMending = true;
             i--;
         }
     }
-    itemStack.getOrCreateTag().put("Enchantments", enchantmentTags);
+    if (removedMending) {
+        itemStack.getOrCreateTag().put("Enchantments", enchantmentTags);
+    }
 })
