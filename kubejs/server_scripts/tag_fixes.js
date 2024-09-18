e96cb093892ffb9b82924431523d6cad1b830f90
qxZap <mileamihai@outlook.it>
2024-09-18 20:18:54 +0300

    removed tag fix

--- kubejs/server_scripts/tag_fixes.js
+++ kubejs/server_scripts/tag_fixes.js
@@ -9,5 +9,4 @@ ServerEvents.tags('item', event => {
     event.removeAll('fabric:mineable/needs_tool_level_4')
 
     event.remove('quark:vertical_slabs', 'outer_end:azure_vertical_slab')
-    event.remove('enlightened_end:cerulean_stalk_blocks', 'enlightened_end:cerulean_stalk_block')
 });
