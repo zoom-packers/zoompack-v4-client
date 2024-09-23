fad5860941bd8b97945ca9925c62b62c57c2f018
panda <pandaismyname1@gmail.com>
2024-09-23 21:14:27 +0300

    Fixed Anticheat error

--- kubejs/client_scripts/anticheat.js
+++ kubejs/client_scripts/anticheat.js
@@ -1,6 +1,6 @@
 function getModList() {
-    const modList = Platform.getMods();
-    const result = [];
+    let modList = Platform.getMods();
+    let result = [];
     modList.forEach((k,v) => {
         result.push({
             id: v.getId() ?? "",
@@ -12,8 +12,8 @@ function getModList() {
 }
 
 function getResourcePackList() {
-    const minecraftClass = Java.loadClass('net.minecraft.client.Minecraft');
-    const resourcePackList = minecraftClass.getInstance().getResourcePackRepository().getSelectedPacks();
+    let minecraftClass = Java.loadClass('net.minecraft.client.Minecraft');
+    let resourcePackList = minecraftClass.getInstance().getResourcePackRepository().getSelectedPacks();
     return resourcePackList.map(resourcePack => {
         return {
             id: resourcePack.getId() ?? "",
@@ -24,17 +24,16 @@ function getResourcePackList() {
 }
 
 function buildServerPackages() {
-    const modList = getModList();
-    const resourcePackList = getResourcePackList();
-    const batchCount = 100;
-    const modBatches = [];
-    const resourcePackBatches = [];
+    let modList = getModList();
+    let resourcePackList = getResourcePackList();
+    let batchCount = 100;
+    let modBatches = [];
+    let resourcePackBatches = [];
     for (let i = 0; i < modList.length; i += batchCount) {
-        modBatches.push(modList.slice(i, i + batchCount));
+        let maxIndex = Math.min(i + batchCount, modList.length);
+        modBatches.push(modList.slice(i, maxIndex));
     }
-    for (let i = 0; i < resourcePackList.length; i += batchCount) {
-        resourcePackBatches.push(resourcePackList.slice(i, i + batchCount));
-    }
+    resourcePackBatches = [resourcePackList];
     return {
         modBatches: modBatches,
         resourcePackBatches: resourcePackBatches
@@ -42,12 +41,12 @@ function buildServerPackages() {
 }
 
 NetworkEvents.dataReceived('anticheat:check', function (event) {
-    const batches = buildServerPackages();
-    const minBatches = Math.min(batches.modBatches.length, batches.resourcePackBatches.length);
-    const maxBatches = Math.max(batches.modBatches.length, batches.resourcePackBatches.length);
-    const mostBatches = batches.modBatches.length > batches.resourcePackBatches.length ? 'mods' : 'resource packs';
+    let batches = buildServerPackages();
+    let minBatches = Math.min(batches.modBatches.length, batches.resourcePackBatches.length);
+    let maxBatches = Math.max(batches.modBatches.length, batches.resourcePackBatches.length);
+    let mostBatches = batches.modBatches.length > batches.resourcePackBatches.length ? 'mods' : 'resource packs';
     for (let i = 0; i < minBatches; i++) {
-        const serverPackage = {
+        let serverPackage = {
             mods: batches.modBatches[i],
             resourcePacks: batches.resourcePackBatches[i]
         };
@@ -55,7 +54,7 @@ NetworkEvents.dataReceived('anticheat:check', function (event) {
     }
     if (mostBatches === 'mods') {
         for (let i = minBatches; i < maxBatches; i++) {
-            const serverPackage = {
+            let serverPackage = {
                 mods: batches.modBatches[i],
                 resourcePacks: []
             };
@@ -63,7 +62,7 @@ NetworkEvents.dataReceived('anticheat:check', function (event) {
         }
     } else {
         for (let i = minBatches; i < maxBatches; i++) {
-            const serverPackage = {
+            let serverPackage = {
                 mods: [],
                 resourcePacks: batches.resourcePackBatches[i]
             };
