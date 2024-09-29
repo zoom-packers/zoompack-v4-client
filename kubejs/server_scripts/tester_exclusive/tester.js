7f27024d98c3defbcfe085e6b57b624ada706716
qxZap <mileamihai@outlook.it>
2024-09-29 13:04:56 +0300

    updated rewarders map

--- kubejs/server_scripts/tester_exclusive/tester.js
+++ kubejs/server_scripts/tester_exclusive/tester.js
@@ -1,6 +1,26 @@
 let TESTER_MAP = {
-    'qxZap' : 1
+    'qxZap' : 2,
+    'Pandaismyname1' : 2,
+    'MihaiAleXD' : 6,
+    'Skydrake' : 5,
+    'Rp__YT' : 5,
+    'TommyTonny' : 5,
+    'GeniusM' : 7,
+    'LikeInReal' : 2,
+    'Shtratulation' : 1
 };
+
+// Emi: 2 points (ISS-01, ISS-03)
+// Mihai: 6 points (ISS-02, ISS-06, ISS-30, ISS-31, ISS-33, ISS-34)
+// Milea: 2 points (ISS-04, ISS-11)
+// Marius: 5 points (ISS-05, ISS-09, ISS-10, ISS-19, ISS-20)
+// RP: 5 points (ISS-07, ISS-08, ISS-24, ISS-26)
+// Tommy: 5 points (ISS-11, ISS-12, ISS-13, ISS-14, ISS-27)
+// Paul: 7 points (ISS-15, ISS-16, ISS-17, ISS-18, ISS-25, ISS-28, ISS-32)
+// Edi: 2 points (ISS-21, ISS-22)
+// Decebal: 1 point (ISS-23) >>not added yet
+// Madalin: 1 point (ISS-29)
+// 24/09/2024
 // TODO: change points for each for release
 
 let TESTER_REWARDS_TAG = 'TESTER_REWARDED_AUTO_FEATURES';
