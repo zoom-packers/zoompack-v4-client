c4b8554cfaa833ef5db75abe357e37bc15c92b75
qxZap <mileamihai@outlook.it>
2024-10-02 18:43:38 +0300

    botezatu loot tables

--- generate_botezatu_loot_tables.py
+++ generate_botezatu_loot_tables.py
@@ -18,59 +18,68 @@ mods = [{
     'mod_id' : 'fantasy_armor',
     'pieces' : pieces_classic,
     'prefixes' : {
+        # uncommon
         'eclipse_soldier' : {
             'common' : 1,
             'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'rare' : 0,
+            'legendary' : 0
         },
+        # rare
         'dragonslayer' : {
-            'common' : 1,
-            'uncommon' : 1,
+            'common' : 0,
+            'uncommon' : 0,
             'rare' : 1,
-            'legendary' : 1
+            'legendary' : 0
         },
+        # rare
         'hero' : {
-            'common' : 1,
-            'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'common' : 0,
+            'uncommon' : 0,
+            'rare' : 2,
+            'legendary' : 0
         },
+        # rare
         'golden_horns' : {
-            'common' : 1,
-            'uncommon' : 1,
+            'common' : 0,
+            'uncommon' : 0,
             'rare' : 1,
-            'legendary' : 1
+            'legendary' : 0
         },
+        # common
         'thief' : {
-            'common' : 1,
+            'common' : 10,
             'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'rare' : 0,
+            'legendary' : 0
         },
+        # not lootable
         'wandering_wizard' : {
-            'common' : 1,
-            'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'common' : 0,
+            'uncommon' : 0,
+            'rare' : 0,
+            'legendary' : 0
         },
+        # uncommon
         'chess_board_knight' : {
             'common' : 1,
             'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'rare' : 0,
+            'legendary' : 0
         },
+        # legendary
         'dark_lord' : {
-            'common' : 1,
-            'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'common' : 0,
+            'uncommon' : 0,
+            'rare' : 0,
+            'legendary' : 4
         },
+        # legendary
         'sunset_wings': {
-            'common' : 1,
-            'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'common' : 0,
+            'uncommon' : 0,
+            'rare' : 0,
+            'legendary' : 4
         },
     },
     'prefix_suffix_bridge' : '_'
@@ -79,59 +88,68 @@ mods = [{
     'mod_id' : 'armoroftheages',
     'pieces' : pieces_wrong,
     'prefixes' : {
+        # not lootable
         'anubis' : {
-            'common' : 1,
-            'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'common' : 0,
+            'uncommon' : 0,
+            'rare' : 0,
+            'legendary' : 0
         },
+        # common
         'centurion' : {
-            'common' : 1,
+            'common' : 10,
             'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'rare' : 0,
+            'legendary' : 0
         },
+        # not lootable
         'holy' : {
-            'common' : 1,
-            'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'common' : 0,
+            'uncommon' : 0,
+            'rare' : 0,
+            'legendary' : 0
         },
+        # common
         'iron_plate' : {
-            'common' : 1,
+            'common' : 10,
             'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'rare' : 0,
+            'legendary' : 0
         },
+        # common
         'japanese_light' : {
-            'common' : 1,
+            'common' : 10,
             'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'rare' : 0,
+            'legendary' : 0
         },
+        # common
         'o_yoroi' : {
-            'common' : 1,
+            'common' : 10,
             'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'rare' : 0,
+            'legendary' : 0
         },
+        # rare
         'pharaoh' : {
-            'common' : 1,
-            'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'common' : 0,
+            'uncommon' : 0,
+            'rare' : 4,
+            'legendary' : 0
         },
+        # legendary
         'quetzalcoatl' : {
-            'common' : 1,
-            'uncommon' : 1,
-            'rare' : 1,
+            'common' : 0,
+            'uncommon' : 0,
+            'rare' : 0,
             'legendary' : 1
         },
+        # not lootable
         'raijin': {
-            'common' : 1,
-            'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'common' : 0,
+            'uncommon' : 0,
+            'rare' : 0,
+            'legendary' : 0
         },
     },
     'prefix_suffix_bridge' : '_armor_'
@@ -140,33 +158,44 @@ mods = [{
     'mod_id' : 'nether_armory',
     'pieces' : pieces_classic,
     'prefixes' : {
+        # legendary
         'infernal_gladiator' : {
-            'common' : 1,
-            'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'common' : 0,
+            'uncommon' : 0,
+            'rare' : 0,
+            'legendary' : 2
         },
+        # uncommon
         'warped_fungus' : {
             'common' : 1,
             'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'rare' : 0,
+            'legendary' : 0
         },
+        # uncommon
         'crimson_fungus' : {
             'common' : 1,
             'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'rare' : 0,
+            'legendary' : 0
         },
+        # uncommon
         'obsidian': {
             'common' : 1,
             'uncommon' : 1,
-            'rare' : 1,
-            'legendary' : 1
+            'rare' : 0,
+            'legendary' : 0
         },
     },
     'prefix_suffix_bridge' : '_armor_'
 }]
+
+rarity_colors = {
+    'legendary' : 'gold',
+    'rare' : 'dark_purple',
+    'uncommon' : 'green',
+    'common': 'white'
+}
 
 for loot_table_tier in loot_tiers:
     file_to_use = f'{botezatu_folder_location}/cosmetics_{loot_table_tier}.json'
@@ -201,6 +230,10 @@ for loot_table_tier in loot_tiers:
                             {
                                 'function' : "minecraft:set_nbt",
                                 "tag": """{affix_data:{affixes:{"apotheosis:armor/attribute/ironforged":0.0f}}}"""
+                            },
+                            {
+                                "function": "set_nbt",
+                                "tag": "{display:{Lore:['{\"text\":\"This is a cosmetic item granted by Catalin Botezatu\",\"color\":\"dark_green\",\"italic\":false}', '{\"text\":\"Rarity: \",\"color\":\"yellow\",\"bold\":true,\"italic\":false,\"extra\":[{\"text\":\""+ loot_table_tier.upper() +"\",\"color\":\""+ rarity_colors[loot_table_tier] +"\",\"bold\":true,\"italic\":false}]}']},HideFlags:63}"
                             }
                         ]
                     })
