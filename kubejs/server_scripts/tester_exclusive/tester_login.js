e239d6344a96f98d77706a906768e8fc61837ceb
qxZap <mileamihai@outlook.it>
2024-10-02 18:46:17 +0300

    updated tester points

--- kubejs/server_scripts/tester_exclusive/tester_login.js
+++ kubejs/server_scripts/tester_exclusive/tester_login.js
@@ -1,26 +1,16 @@
 let TESTER_MAP = {
     'qxZap' : 2,
     'Pandaismyname1' : 2,
-    'MihaiAleXD' : 6,
-    'Skydrake' : 5,
-    'Rp__YT' : 5,
-    'TommyTonny' : 5,
-    'GeniusM' : 7,
+    'MihaiAleXD' : 7,
+    'Skydrake' : 6,
+    'Rp__YT' : 4,
+    'TommyTonny' : 6,
+    'GeniusM' : 10,
     'LikeInReal' : 2,
     'Shtratulation' : 1
+    // Missing decebal
 };
 
-// Emi: 2 points (ISS-01, ISS-03)
-// Mihai: 6 points (ISS-02, ISS-06, ISS-30, ISS-31, ISS-33, ISS-34)
-// Milea: 2 points (ISS-04, ISS-11)
-// Marius: 5 points (ISS-05, ISS-09, ISS-10, ISS-19, ISS-20)
-// RP: 5 points (ISS-07, ISS-08, ISS-24, ISS-26)
-// Tommy: 5 points (ISS-11, ISS-12, ISS-13, ISS-14, ISS-27)
-// Paul: 7 points (ISS-15, ISS-16, ISS-17, ISS-18, ISS-25, ISS-28, ISS-32)
-// Edi: 2 points (ISS-21, ISS-22)
-// Decebal: 1 point (ISS-23) >>not added yet
-// Madalin: 1 point (ISS-29)
-// 24/09/2024
 // TODO: change points for each for release
 
 let TESTER_REWARDS_TAG = 'TESTER_REWARDED_AUTO_FEATURES';
