b906414dff65291e0656d42c14efae162fa65103
qxZap <mileamihai@outlook.it>
2024-10-07 23:06:38 +0300

    typo

--- kubejs/server_scripts/recipe_removed.js
+++ kubejs/server_scripts/recipe_removed.js
@@ -1,7 +1,7 @@
 ServerEvents.recipes(event => {
     // TBA
     event.remove({ output: 'minecraft:diamond' })
-    event.remove({ output: 'slotmachinemod:slot_machin' })
+    event.remove({ output: 'slotmachinemod:slot_machine' })
     event.remove({ output: 'minecraft:diamond_block', input: "minecraft:emerald_block" })
     event.remove({ output: 'sophisticatedbackpacks:inception_upgrade' })
 })
