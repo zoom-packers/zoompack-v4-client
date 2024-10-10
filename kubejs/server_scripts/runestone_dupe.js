64b24e2bfb7ffc6f95ca99100ebf00a160f5ed43
panda <pandaismyname1@gmail.com>
2024-10-10 10:57:24 +0300

    Runestone Duplication Script

---
+++ kubejs/server_scripts/runestone_dupe.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+    event.shaped("2x irons_spellbooks:blank_rune", [
+        "AAA",
+        "ABA",
+        "AAA"
+    ], {
+        A: "minecraft:diamond",
+        B: "irons_spellbooks:blank_rune"
+    })
+});
