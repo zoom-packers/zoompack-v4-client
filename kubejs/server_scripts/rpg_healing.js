e19d0794d01a769747ba587de2d7d702a27059c9
qxZap <mileamihai@outlook.it>
2024-10-20 20:21:26 +0300

    performance improvement for rpg healing

--- kubejs/server_scripts/rpg_healing.js
+++ kubejs/server_scripts/rpg_healing.js
@@ -3,21 +3,28 @@ function setPlayerInCombat(player){
     player.persistentData.ON_COMBAT = true;
 }
 
-PlayerEvents.tick(event => {
-    let player = event.player;
+let rpgHealingTickInterval = 20;
+let rpgHealingTimer = 0;
 
-    if (!player.persistentData.lastDamageTime) {
-        player.persistentData.lastDamageTime = 0;
-    }
-
-    if (player.persistentData.lastDamageTime + 100 < player.level.time) {
-        player.persistentData.ON_COMBAT = false;
-    }
-
-    if (!player.persistentData.ON_COMBAT && player.foodData.foodLevel >= 20) {
-        let healAmount = player.maxHealth * (Math.random() * (0.10 - 0.05) + 0.05);
-        setPlayerInCombat(player);
-        player.heal(healAmount);
+ServerEvents.tick((event) => {
+    rpgHealingTimer++;
+    if (rpgHealingTimer > rpgHealingTickInterval) {
+        event.server.players.forEach(player => {
+            if (!player.persistentData.lastDamageTime) {
+                player.persistentData.lastDamageTime = 0;
+            }
+        
+            if (player.persistentData.lastDamageTime + 100 < player.level.time) {
+                player.persistentData.ON_COMBAT = false;
+            }
+        
+            if (!player.persistentData.ON_COMBAT && player.foodData.foodLevel >= 20) {
+                let healAmount = player.maxHealth * (Math.random() * (0.10 - 0.05) + 0.05);
+                setPlayerInCombat(player);
+                player.heal(healAmount);
+            }
+        });
+        rpgHealingTimer = 0;
     }
 });
 
