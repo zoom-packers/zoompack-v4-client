413f6e3222523e5336660624f9bb9a9ce9d7d904
qxZap <mileamihai@outlook.it>
2024-11-01 20:04:00 +0200

    improvement to backpack limiter

--- kubejs/server_scripts/backpack_limiter.js
+++ kubejs/server_scripts/backpack_limiter.js
@@ -14,30 +14,21 @@ let backpackLimiterIds = [
 
 let backpackLimiterPlayerTicks = {}
 let backpackLimiterPreviousBackpackCount = {}
-PlayerEvents.tick(event => {
-    let player = event.player
-    let server = event.server
-    let playerUUID = player.uuid
-    if (!backpackLimiterPlayerTicks[playerUUID]) {
-        backpackLimiterPlayerTicks[playerUUID] = 0
-        backpackLimiterPreviousBackpackCount[playerUUID] = 0
-    }
-    backpackLimiterPlayerTicks[playerUUID]++
-    if (backpackLimiterPlayerTicks[playerUUID] % 20 !== 0) {
-        return;
-    }
 
+function getPlayerBackpacksTotal(player){
     let inventory = player.inventory.items;
     let armors = player.inventory.armor;
     let backSlotDefinition = backpackLimiterCuriosAPI.getCuriosInventory(player).resolve().get().getCurios().back;
     let stacks = backSlotDefinition.getStacks();
     let slots = backSlotDefinition.getSlots();
     let backSlots = [];
+
     for (let i = 0; i < slots; i++) {
         backSlots.push(stacks.getStackInSlot(i));
     }
 
     let backpackCount = 0;
+
     for (let i = 0; i < inventory.size(); i++) {
         let itemStack = inventory.get(i);
         if (backpackLimiterCheckForBackpack(itemStack)) {
@@ -57,24 +48,88 @@ PlayerEvents.tick(event => {
         }
     }
 
-    if (backpackCount > backpackLimiterMaxBackpacks) {
-        player.sendSystemMessage("§4You are carrying too many backpacks! " +
-            "The limit is " + backpackLimiterMaxBackpacks + " backpacks at a time." +
-            "§r §2Drop some backpacks to unburden yourself.§r " +
-            "§3Don't forget to check your Curios slots too.§r");
+    return backpackCount;
+}
+
+ItemEvents.pickedUp(event=>{
+    let item = event.getItem();
+    let item_id =item.item.getId();
+
+    let player = event.player
+    let server = event.server
+    let playerUUID = player.uuid;
+
+    if (backpackLimiterIds.includes(item_id)){
+        if(!backpackLimiterPreviousBackpackCount[playerUUID]){
+            backpackLimiterPreviousBackpackCount[playerUUID] = getPlayerBackpacksTotal(player);
+        }
+        else{
+            backpackLimiterPreviousBackpackCount[playerUUID] +=1;
+        }
+    }   
+
+});
+
+ItemEvents.dropped (event=>{
+    let item = event.getItem();
+    let item_id =item.item.getId();
+
+    let player = event.player
+    let server = event.server
+    let playerUUID = player.uuid;
+    if (backpackLimiterIds.includes(item_id)){
+        if(backpackLimiterPreviousBackpackCount[playerUUID]){
+            backpackLimiterPreviousBackpackCount[playerUUID] = getPlayerBackpacksTotal(player);
+        }
+        else{
+            backpackLimiterPreviousBackpackCount[playerUUID] -=1;
+        }
+        
+        if (backpackLimiterPreviousBackpackCount[playerUUID]<=backpackLimiterMaxBackpacks){
+            server.runCommandSilent(`/attribute ${player.getName().getString()} minecraft:generic.movement_speed modifier remove ${backpackLimiterMagicUUID}`);
+            player.sendSystemMessage("§3You are no longer carrying too many backpacks.§r " +
+                "§2You can now move at full speed.§r");
+        }
     }
 
-    if (backpackCount > backpackLimiterMaxBackpacks && backpackLimiterPreviousBackpackCount[playerUUID] <= backpackLimiterMaxBackpacks) {
-        //Apply slowness effect
+});
+
+
+function clearPlayerStun(server, player){
+    server.runCommandSilent(`/attribute ${player.getName().getString()} minecraft:generic.movement_speed modifier remove ${backpackLimiterMagicUUID}`);
+    player.sendSystemMessage("§3You are no longer carrying too many backpacks.§r " +
+        "§2You can now move at full speed.§r");
+}
+
+function stunPlayer(server, player){
+    player.sendSystemMessage("§4You are carrying too many backpacks! " +
+        "The limit is " + backpackLimiterMaxBackpacks + " backpacks at a time." +
+        "§r §2Drop some backpacks to unburden yourself.§r " +
+        "§3Don't forget to check your Curios slots too.§r");
         server.runCommandSilent(`/attribute ${player.getName().getString()} minecraft:generic.movement_speed modifier add ${backpackLimiterMagicUUID} "Backpack Penalty" -100 add`);
+}
+
+PlayerEvents.inventoryChanged(event => {
+    let item = event.getItem();
+    let item_id =item.item.getId();
+
+    if (backpackLimiterIds.includes(item_id)){
+        let player = event.player
+        let server = event.server
+        let playerUUID = player.uuid;
+
+        if (getPlayerBackpacksTotal(player) > backpackLimiterMaxBackpacks) {
+            stunPlayer(server, player);
+        }
+        else{
+            if (backpackLimiterPreviousBackpackCount[playerUUID]<=backpackLimiterMaxBackpacks){
+                clearPlayerStun(server, player);
+            }
+        }
+
+        backpackLimiterPreviousBackpackCount[playerUUID] = backpackCount;
     }
-    if (backpackCount <= backpackLimiterMaxBackpacks && backpackLimiterPreviousBackpackCount[playerUUID] > backpackLimiterMaxBackpacks) {
-        //Remove slowness effect
-        server.runCommandSilent(`/attribute ${player.getName().getString()} minecraft:generic.movement_speed modifier remove ${backpackLimiterMagicUUID}`);
-        player.sendSystemMessage("§3You are no longer carrying too many backpacks.§r " +
-            "§2You can now move at full speed.§r");
-    }
-    backpackLimiterPreviousBackpackCount[playerUUID] = backpackCount;
+    
 });
 
 function backpackLimiterCheckForBackpack(itemStack) {
