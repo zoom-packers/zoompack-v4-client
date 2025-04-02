c27dc473ca717242882589616d906848ed2a5d95
panda <pandaismyname1@gmail.com>
2025-04-02 17:17:33 +0300

    Armuri? Armuri!

--- tools/js_gen/dim_builder/magic_armors.ts
+++ tools/js_gen/dim_builder/magic_armors.ts
@@ -23,20 +23,31 @@ const geoPath = path.join(assetsPath, "geo")
 const animPath = path.join(assetsPath, "animations")
 const modelsPath = path.join(assetsPath, "models", "armor")
 const texturesPath = path.join(assetsPath, "textures")
+
 const pyromancerGeoPath = path.join(geoPath, "pyromancer_armor.geo.json")
 const cryomancerGeoPath = path.join(geoPath, "cryomancer_armor.geo.json")
 const electromancerGeoPath = path.join(geoPath, "electromancer_armor.geo.json")
 const archevokerGeoPath = path.join(geoPath, "archevoker_armor.geo.json")
-const cultistGeoPath = path.join(geoPath, "cultist_armor.geo.json")
-const shadowwalkerGeoPath = path.join(geoPath, "shadowwalker_armor.geo.json")
+const cultistGeoPath = path.join(geoPath, "hemomancer_armor.geo.json")
+const shadowwalkerGeoPath = path.join(geoPath, "chorusguardian_armor.geo.json")
 const priestGeoPath = path.join(geoPath, "priest_armor.geo.json")
-const plaguedGeoPath = path.join(geoPath, "plagued_armor.geo.json")
+const plaguedGeoPath = path.join(geoPath, "druid_armor.geo.json")
 
 const pyromancerAnimPath = path.join(animPath, "pyromancer_armor.animation.json")
+const cryomancerAnimPath = path.join(animPath, "cryomancer_armor.animation.json")
+const electromancerAnimPath = path.join(animPath, "electromancer_armor.animation.json")
+const archevokerAnimPath = path.join(animPath, "archevoker_armor.animation.json")
+const cultistAnimPath = path.join(animPath, "hemomancer_armor.animation.json")
+const shadowwalkerAnimPath = path.join(animPath, "chorusguardian_armor.animation.json")
+const priestAnimPath = path.join(animPath, "priest_armor.animation.json")
+const plaguedAnimPath = path.join(animPath, "druid_armor.animation.json")
 
 
 function getModelPath(name: string, piece: string): string {
     return path.join(modelsPath, `${name}_${piece}.json`);
+}
+function getDefaultModelPath() {
+    return path.join(modelsPath, "item_template.json");
 }
 function getTexturePath(name: string, piece: string): string {
     return path.join(texturesPath, `${name}_${piece}.png`);
@@ -189,34 +200,34 @@ const pyromancerVariants = ["head", "chest", "legs", "feet"].map((slot) => creat
 const cryomancerVariants = ["head", "chest", "legs", "feet"].map((slot) => createArmorVariant("cryomancer", slot, cryomancerCraftingMaterial));
 const electromancerVariants = ["head", "chest", "legs", "feet"].map((slot) => createArmorVariant("electromancer", slot, electromancerCraftingMaterial));
 const archevokerVariants = ["head", "chest", "legs", "feet"].map((slot) => createArmorVariant("archevoker", slot, archevokerCraftingMaterial));
-const cultistVariants = ["head", "chest", "legs", "feet"].map((slot) => createArmorVariant("cultist", slot, cultistCraftingMaterial));
-const shadowwalkerVariants = ["head", "chest", "legs", "feet"].map((slot) => createArmorVariant("shadowwalker", slot, shadowwalkerCraftingMaterial));
+const cultistVariants = ["head", "chest", "legs", "feet"].map((slot) => createArmorVariant("hemomancer", slot, cultistCraftingMaterial));
+const shadowwalkerVariants = ["head", "chest", "legs", "feet"].map((slot) => createArmorVariant("chorusguardian", slot, shadowwalkerCraftingMaterial));
 const priestVariants = ["head", "chest", "legs", "feet"].map((slot) => createArmorVariant("priest", slot, priestCraftingMaterial));
-const plaguedVariants = ["head", "chest", "legs", "feet"].map((slot) => createArmorVariant("plagued", slot, plaguedCraftingMaterial));
+const plaguedVariants = ["head", "chest", "legs", "feet"].map((slot) => createArmorVariant("druid", slot, plaguedCraftingMaterial));
 
 const pyromancerChromas = [
     ChromaCreator.create("#00ff00", "#000000", 0.1, "linear"),
 ];
 const cryomancerChromas = [
-    ChromaCreator.create("#182024", "#000000", 0.1, "linear"),
+    ChromaCreator.create("#00ff00", "#000000", 0.1, "linear"),
 ];
 const electromancerChromas = [
-    ChromaCreator.create("#f4a88d", "#000000", 0.1, "linear"),
+    ChromaCreator.create("#EE8F1F", "#000000", 0.1, "linear"),
 ];
 const archevokerChromas = [
-    ChromaCreator.create("#ffbb0f", "#000000", 0.1, "linear"),
+    ChromaCreator.create("#ff0000", "#000000", 0.2, "linear"),
 ];
 const cultistChromas = [
-    ChromaCreator.create("#5e5c4e", "#000000", 0.1, "linear"),
+    ChromaCreator.create("#00ff00", "#000000", 0.1, "linear"),
 ];
 const shadowwalkerChromas = [
-    ChromaCreator.create("#5b3138", "#000000", 0.1, "linear"),
+    ChromaCreator.create("#00ff00", "#000000", 0.1, "linear"),
 ];
 const priestChromas = [
-    ChromaCreator.create("#ffbb0f", "#000000", 0.05, "linear"),
+    ChromaCreator.create("#00ff00", "#000000", 0.1, "linear"),
 ];
 const plaguedChromas = [
-    ChromaCreator.create("#ebe9b5", "#000000", 0.1, "linear"),
+    ChromaCreator.create("#00ff00", "#000000", 0.1, "linear"),
 ];
 
 function createArmorySet(name: string, geoPath: string, animPath: string, defaultAnims: DefaultAnimation[] | null, chromas: ChromaKeyOperation[], armorVariants: ArmorVariant[]): GeckoArmorArmoryEntry {
@@ -230,7 +241,7 @@ function createArmorySet(name: string, geoPath: string, animPath: string, defaul
         .withGeoPaths([geoPath])
         .withAnimationPaths([animPath])
         .withDefaultAnimations(defaultAnims)
-        .withModelPaths(["helmet", "chestplate", "leggings", "boots"].map((piece) => getModelPath(name, piece)))
+        .withModelPaths(["helmet", "chestplate", "leggings", "boots"].map((piece) => getDefaultModelPath()))
         .withTextures(textures)
         .withAdditionalTextures([
             {
@@ -249,11 +260,46 @@ export const magicArmorSets = [
             piece: "chestplate",
         }
     ], pyromancerChromas, pyromancerVariants),
-    // createArmorySet("cryomancer", cryomancerGeoPath, cryomancerChromas, cryomancerVariants),
-    // createArmorySet("electromancer", electromancerGeoPath, electromancerChromas, electromancerVariants),
-    // createArmorySet("archevoker", archevokerGeoPath, archevokerChromas, archevokerVariants),
-    // createArmorySet("cultist", cultistGeoPath, cultistChromas, cultistVariants),
-    // createArmorySet("shadowwalker", shadowwalkerGeoPath, shadowwalkerChromas, shadowwalkerVariants),
-    // createArmorySet("priest", priestGeoPath, priestChromas, priestVariants),
-    // createArmorySet("plagued", plaguedGeoPath, plaguedChromas, plaguedVariants),
+    createArmorySet("cryomancer", cryomancerGeoPath, cryomancerAnimPath, [
+        {
+            name: "animation.deco.loop",
+            piece: "chestplate",
+        }
+    ], cryomancerChromas, cryomancerVariants),
+    createArmorySet("electromancer", electromancerGeoPath, electromancerAnimPath, [
+        {
+            name: "animation.deco.loop",
+            piece: "chestplate",
+        }
+    ], electromancerChromas, electromancerVariants),
+    createArmorySet("archevoker", archevokerGeoPath, archevokerAnimPath, [
+        {
+            name: "animation.deco.loop",
+            piece: "chestplate",
+        }
+    ], archevokerChromas, archevokerVariants),
+    createArmorySet("hemomancer", cultistGeoPath, cultistAnimPath, [
+        {
+            name: "animation.deco.loop",
+            piece: "chestplate",
+        }
+    ], cultistChromas, cultistVariants),
+    createArmorySet("chorusguardian", shadowwalkerGeoPath, shadowwalkerAnimPath, [
+        {
+            name: "animation.deco.loop",
+            piece: "chestplate",
+        }
+    ], shadowwalkerChromas, shadowwalkerVariants),
+    createArmorySet("priest", priestGeoPath, priestAnimPath, [
+        {
+            name: "animation.deco.loop",
+            piece: "chestplate",
+        }
+    ], priestChromas, priestVariants),
+    createArmorySet("druid", plaguedGeoPath, plaguedAnimPath, [
+        {
+            name: "animation.deco.loop",
+            piece: "chestplate",
+        }
+    ], plaguedChromas, plaguedVariants)
 ]
