4d225c2b16f9c2ddc0f5e12c574238ac4d8980f4
qxZap <mileamihai@outlook.it>
2025-04-12 12:07:11 +0300

    spells slight rebalance

--- defaultconfigs/balance_spells.py
+++ defaultconfigs/balance_spells.py
@@ -10,14 +10,25 @@ new_lines = []
 for line in lines:
     # Write here logic for changes
     # Example to reduce cooldown by 25%
-    if 'CooldownInSeconds' in line:
-        time_str = line.split('= ')[1].split('\n')[0]
-        time_float = float(time_str)
+    # if 'CooldownInSeconds' in line:
+    #     time_str = line.split('= ')[1].split('\n')[0]
+    #     time_float = float(time_str)
 
-        new_time_float = round(time_float*0.75,1)
-        new_time_str = str(new_time_float)
+    #     new_time_float = round(time_float*0.75,1)
+    #     new_time_str = str(new_time_float)
 
-        new_lines.append(line.replace(f'= {time_str}',f'= {new_time_str}'))
+    #     new_lines.append(line.replace(f'= {time_str}',f'= {new_time_str}'))
+    # else:
+    #     new_lines.append(line)
+
+    # Exmple to reduce spell power multiplier by 30%
+    if 'SpellPowerMultiplier' in line:
+        spell_m_str = line.split('= ')[1].split('\n')[0]
+        spell_m_float = float(spell_m_str)
+
+        new_spell_m_float = round(spell_m_float*0.7,4)
+        new_spell_m_str = str(new_spell_m_float)
+        new_lines.append(line.replace(f'= {spell_m_str}',f'= {new_spell_m_str}'))
     else:
         new_lines.append(line)
 
