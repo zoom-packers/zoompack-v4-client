194ab1738a7c9dbf31baf8e30256d5a7900dea43
qxZap <mileamihai@outlook.it>
2025-04-13 15:57:49 +0300

    stomp patch

--- kubejs/server_scripts/spells/spells_level_limiter.js
+++ kubejs/server_scripts/spells/spells_level_limiter.js
@@ -1,3 +1,7 @@
+
+const CHAIN_LIGHTNING_MAX_RANGE = 20;
+const STOMP_MAX_STEPS = 20;
+
 let PmmoAPIUtils = Java.loadClass('harmonised.pmmo.api.APIUtils');
 let PmmoReqType = Java.loadClass('harmonised.pmmo.api.enums.ReqType');
 let pmmoLevelsPerSpellLevel = 5;
@@ -18,9 +22,16 @@ EntityEvents.spawned(spellEntity);
 function spellEntity(event){
     const entity = event.getEntity();
     const type = entity.getType();
+    
     if(type === "irons_spellbooks:chain_lightning"){
-        if (entity.range>20){
-            entity.range = 20;
+        if (entity.range>CHAIN_LIGHTNING_MAX_RANGE){
+            entity.range = CHAIN_LIGHTNING_MAX_RANGE;
+        }
+    }
+
+    if(type === "irons_spellbooks:stomp_aoe"){
+        if (entity.nbt.getInt('maxSteps')>STOMP_MAX_STEPS){
+            entity.mergeNbt({'maxSteps':STOMP_MAX_STEPS});
         }
     }
 }
