151fad2a8a1bedc05b6a13d1ec192319a13b9b37
qxZap <mileamihai@outlook.it>
2025-04-30 21:28:33 +0300

    balanced cosmetics script

--- kubejs/startup_scripts/balance_cosmetics.js
+++ kubejs/startup_scripts/balance_cosmetics.js
@@ -45,6 +45,101 @@ ItemEvents.modification((event) => {
         item.armorToughness = 0;
         item.maxDamage = 696;
     });
+    event.modify("fantasy_armor:fog_guard_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:dark_cover_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:spark_of_dawn_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:golden_execution_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:forgotten_trace_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:redeemer_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:winned_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:gilded_hunt_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:lady_maria_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:crucible_knight_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:ronin_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:evening_ghost_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:malenia_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:old_knight_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:silver_knight_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:dead_gladiator_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:flesh_of_the_feaster_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:wind_worshipper_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:twinned_helmet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
     event.modify("fantasy_armor:eclipse_soldier_chestplate", item => {
         item.armorProtection = -1;
         item.armorToughness = 0;
@@ -86,6 +181,101 @@ ItemEvents.modification((event) => {
         item.maxDamage = 696;
     });
     event.modify("fantasy_armor:sunset_wings_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:fog_guard_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:dark_cover_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:spark_of_dawn_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:golden_execution_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:forgotten_trace_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:redeemer_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:winned_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:gilded_hunt_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:lady_maria_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:crucible_knight_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:ronin_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:evening_ghost_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:malenia_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:old_knight_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:silver_knight_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:dead_gladiator_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:flesh_of_the_feaster_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:wind_worshipper_chestplate", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:twinned_chestplate", item => {
         item.armorProtection = -1;
         item.armorToughness = 0;
         item.maxDamage = 696;
@@ -135,6 +325,101 @@ ItemEvents.modification((event) => {
         item.armorToughness = 0;
         item.maxDamage = 696;
     });
+    event.modify("fantasy_armor:fog_guard_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:dark_cover_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:spark_of_dawn_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:golden_execution_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:forgotten_trace_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:redeemer_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:winned_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:gilded_hunt_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:lady_maria_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:crucible_knight_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:ronin_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:evening_ghost_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:malenia_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:old_knight_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:silver_knight_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:dead_gladiator_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:flesh_of_the_feaster_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:wind_worshipper_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:twinned_leggings", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
     event.modify("fantasy_armor:eclipse_soldier_boots", item => {
         item.armorProtection = -1;
         item.armorToughness = 0;
@@ -176,6 +461,101 @@ ItemEvents.modification((event) => {
         item.maxDamage = 696;
     });
     event.modify("fantasy_armor:sunset_wings_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:fog_guard_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:dark_cover_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:spark_of_dawn_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:golden_execution_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:forgotten_trace_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:redeemer_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:winned_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:gilded_hunt_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:lady_maria_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:crucible_knight_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:ronin_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:evening_ghost_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:malenia_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:old_knight_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:silver_knight_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:dead_gladiator_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:flesh_of_the_feaster_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:wind_worshipper_boots", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("fantasy_armor:twinned_boots", item => {
         item.armorProtection = -1;
         item.armorToughness = 0;
         item.maxDamage = 696;
@@ -225,6 +605,11 @@ ItemEvents.modification((event) => {
         item.armorToughness = 0;
         item.maxDamage = 696;
     });
+    event.modify("armoroftheages:exalted_aurum_armor_head", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
     event.modify("armoroftheages:anubis_armor_chest", item => {
         item.armorProtection = -1;
         item.armorToughness = 0;
@@ -266,6 +651,11 @@ ItemEvents.modification((event) => {
         item.maxDamage = 696;
     });
     event.modify("armoroftheages:raijin_armor_chest", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("armoroftheages:exalted_aurum_armor_chest", item => {
         item.armorProtection = -1;
         item.armorToughness = 0;
         item.maxDamage = 696;
@@ -315,6 +705,11 @@ ItemEvents.modification((event) => {
         item.armorToughness = 0;
         item.maxDamage = 696;
     });
+    event.modify("armoroftheages:exalted_aurum_armor_legs", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
     event.modify("armoroftheages:anubis_armor_feet", item => {
         item.armorProtection = -1;
         item.armorToughness = 0;
@@ -356,6 +751,11 @@ ItemEvents.modification((event) => {
         item.maxDamage = 696;
     });
     event.modify("armoroftheages:raijin_armor_feet", item => {
+        item.armorProtection = -1;
+        item.armorToughness = 0;
+        item.maxDamage = 696;
+    });
+    event.modify("armoroftheages:exalted_aurum_armor_feet", item => {
         item.armorProtection = -1;
         item.armorToughness = 0;
         item.maxDamage = 696;
