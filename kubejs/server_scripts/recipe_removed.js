091f23fe688b29804067ce083c795ca08741f072
qxZap <mileamihai@outlook.it>
2025-05-01 00:00:12 +0300

    slot machines now craftable again

--- kubejs/server_scripts/recipe_removed.js
+++ kubejs/server_scripts/recipe_removed.js
@@ -3,7 +3,6 @@ ServerEvents.recipes(event => {
     event.remove({ output: 'minecraft:diamond', input: "minecraft:coal_block" })
     event.remove({ output: 'minecolonies:supplychestdeployer' })
     event.remove({ output: 'minecolonies:supplycampdeployer' })
-    event.remove({ output: 'slotmachinemod:slot_machine' })
     event.remove({ output: 'minecraft:diamond_block', input: "minecraft:emerald_block" })
     event.remove({ output: 'sophisticatedbackpacks:inception_upgrade' })
     event.remove({ output: 'theabyss:ring_of_flight' })
