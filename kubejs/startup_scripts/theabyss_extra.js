41de4a71b307a656be3ac1a654bacf2a90525253
qxZap <mileamihai@outlook.it>
2025-10-20 12:15:05 +0300

    abyssaur item to be looted too

--- kubejs/startup_scripts/theabyss_extra.js
+++ kubejs/startup_scripts/theabyss_extra.js
@@ -2,4 +2,10 @@ StartupEvents.registry('item', e => {
     e.create("theabyss:magic_core")
             .maxStackSize(1)
             .displayName('Magic Core')
+    e.create("theabyss:prehistoric_claw")
+            .maxStackSize(1)
+            .displayName('Prehistoric Claw')
+
+
+            
 })
