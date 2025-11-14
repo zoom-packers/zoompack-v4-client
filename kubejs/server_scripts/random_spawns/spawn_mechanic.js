32f438e86be16c878864b40d4b37456fc364a588
qxZap <mileamihai@outlook.it>
2025-11-14 20:22:51 +0200

    spawn mech

--- kubejs/server_scripts/random_spawns/spawn_mechanic.js
+++ kubejs/server_scripts/random_spawns/spawn_mechanic.js
@@ -1,12 +1,35 @@
 const DIM_TARGETS = {
-    'callfromthedepth_:depth': {
-        overwrites:{
-            'callfromthedepth_:riper' : ['callfromthedepth_:marbleextruder'],
-            'callfromthedepth_:rotwalker' : ['callfromthedepth_:marbleextruder'],
-            'callfromthedepth_:roteater' : ['callfromthedepth_:marbleextruder'],
-            'callfromthedepth_:seeker' : ['callfromthedepth_:marbleextruder']
+    // 'theabyss:frost_world':{
+    //     overwrites:{
+    //         'theabyss:ice_knight': [],
+    //     },
+    //     chance : 0.1
+    // },
+    'theabyss:the_abyss': {
+        overwrites: {
+            'theabyss:infceted_cow': ['theabyss:infected_phantom', 'theabyss:lizard'],
+            'theabyss:abyssal_lion': ['theabyss:glow_pug']
         },
-        chance: 0.08
+        chance: 0.03
+    },
+    'callfromthedepth_:depth': {
+        overwrites: {
+            'callfromthedepth_:riper': ['callfromthedepth_:marbleextruder'],
+            'callfromthedepth_:rotwalker': ['callfromthedepth_:marbleextruder'],
+            'callfromthedepth_:roteater': ['callfromthedepth_:marbleextruder'],
+            'callfromthedepth_:seeker': ['callfromthedepth_:marbleextruder']
+        },
+        chance: 0.04
+    },
+    'minecraft:overworld': {
+        overwrites: {
+            'naturalist:butterfly': ['minecraft:sheep', 'minecraft:cow', 'minecraft:chicken', 'minecraft:pig'],
+            'naturalist:snail': ['minecraft:sheep', 'minecraft:cow', 'minecraft:chicken', 'minecraft:pig'],
+            'naturalist:robin': ['minecraft:sheep', 'minecraft:cow', 'minecraft:chicken', 'minecraft:pig'],
+            'naturalist:snake': ['minecraft:sheep', 'minecraft:cow', 'minecraft:chicken', 'minecraft:pig'],
+            'minecraft:horse': ['minecraft:sheep', 'minecraft:cow', 'minecraft:chicken', 'minecraft:pig']
+        },
+        chance: 0.1
     }
 }
 
@@ -20,13 +43,13 @@ function randomFromList(list) {
     return list[index];
 }
 
-EntityEvents.spawned(event=>{
-    const {entity,server} = event;
+EntityEvents.spawned(event => {
+    const { entity, server } = event;
     let dimension = entity.level.dimension.toString();
-    if(DIM_TARGETS[dimension]){
-        if(DIM_TARGETS[dimension].chance>=rollDicesForSpawn()){
+    if (DIM_TARGETS[dimension]) {
+        if (DIM_TARGETS[dimension].chance >= rollDicesForSpawn()) {
             let entityType = entity.getType();
-            if(DIM_TARGETS[dimension].overwrites[entityType]){
+            if (DIM_TARGETS[dimension].overwrites[entityType]) {
                 let whatToSpawnExtra = randomFromList(DIM_TARGETS[dimension].overwrites[entityType]);
                 server.runCommandSilent(`execute in ${dimension} run summon ${whatToSpawnExtra} ${entity.x} ${entity.y} ${entity.z}`);
             }
